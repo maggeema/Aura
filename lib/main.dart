60f9cabbfeb789c12462fdb7127bd4c174bd7d67
Maggie Ma <80849769+maggeema@users.noreply.github.com>
2025-03-24 08:50:52 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'Pages/home_page.dart' as home;
 import 'Pages/settings_page.dart';
 import 'Pages/reviews_page.dart';
@@ -8,7 +11,11 @@ import 'Pages/login_page.dart';
 import 'Pages/map_page.dart';
 import 'Pages/create_account_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -25,7 +32,7 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginPage(),
         '/register': (context) => CreateAccountPage(),
         '/map': (context) => MapPage(),
-        '/home': (context) => home.HomePage(), // Use alias for HomePage
+        '/home': (context) => home.HomePage(),
         '/settings': (context) => SettingsPage(),
         '/reviews': (context) => ReviewsPage(),
       },
